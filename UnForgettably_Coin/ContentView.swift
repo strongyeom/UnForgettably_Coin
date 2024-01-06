1c4470fe3dc723989d4e7887d5922cd6938783af
yeomsungpil <present7256@gmail.com>
2024-01-06 16:04:19 +0900

    Initial Commit

---
+++ UnForgettably_Coin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnForgettably_Coin
+//
+//  Created by 염성필 on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnForgettably_Coin/UnForgettably_CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnForgettably_CoinApp.swift
+//  UnForgettably_Coin
+//
+//  Created by 염성필 on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct UnForgettably_CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
